1d826862ff038dbdefa69e5b021575258e57c883
BalJoseph <48828516+BalJoseph@users.noreply.github.com>
2020-05-08 15:47:54 +0200

    Added Investigation repository

---
+++ Nemesys/Models/IInvestigationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemesys.Models
+{
+    public interface IInvestigationRepository
+    {
+        IEnumerable<Investigation> GetAllInvestigations();
+        Investigation GetInvestigatiosById(int id);
+        void UpdateInvestigation(Investigation investigation);
+        void CreateInvestigation(Investigation investigation);
+        void EditInvestigation(Investigation investigation);
+        void DeleteInvestigation(Investigation investigation);
+    }
+}
