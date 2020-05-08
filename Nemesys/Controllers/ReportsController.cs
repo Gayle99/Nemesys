79f39ac8a313036b8b84138c6c5440f6abc65d1a
BalJoseph <48828516+BalJoseph@users.noreply.github.com>
2020-05-08 18:17:14 +0200

    Add the list all method

--- Nemesys/Controllers/ReportsController.cs
+++ Nemesys/Controllers/ReportsController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nemesys.Models;
+using Nemesys.ViewModels;
 
 namespace Nemesys.Controllers
 {
     public class ReportsController : Controller
     {
+        private readonly IReportRepository _reportRepository;
+
         public IActionResult Index()
         {
-            return View();
+            
 
             var model = new ReportsListViewModel();
+            model.Reports = _reportRepository.GetAllReports().OrderByDescending(x => x.Upvotes);
+            model.ReportsCount = model.Reports.Count();
+
+            return View(model);
         }
     }
 }
--- Nemesys/ViewModels/ReportsListViewModel.cs
+++ Nemesys/ViewModels/ReportsListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nemesys.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,7 @@ namespace Nemesys.ViewModels
 {
     public class ReportsListViewModel
     {
+        public int ReportsCount { get; set; }
+        public IEnumerable<Report> Reports { get; set; }
     }
 }
