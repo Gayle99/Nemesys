05d824c56b7d3fbdc76d47297f8039cc5a1e3a2f
Gayle Cassar <gaylecassar@hotmail.com>
2020-05-08 15:20:22 +0200

    Created report + interface

---
+++ Nemesys/Models/IReportRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nemesys.Models
+{
+    public interface IReportRepository
+    {
+        IEnumerable<Report> GetAllReports();
+        Report GetReportById(int id);
+        void UpdateReport(Report report);
+        void CreateReport(Report report);
+        void EditReport(Report report);
+        void DeleteReport(Report report);
+    }
+}
---
+++ Nemesys/Models/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Nemesys.Models
+{
+    public class Report
+    {
+        public DateTime DateOfCreation { get; set; }
+        public string Location { get; set; }
+        public DateTime DateSpotted { get; set; }
+        public string TypeOfHazard { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+        public int Upvotes { get; set; }
+        
+    }
+}
