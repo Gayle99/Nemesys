90a73bde196aff920b4b9f7fab77bf981780c614
BalJoseph <48828516+BalJoseph@users.noreply.github.com>
2020-05-08 18:35:55 +0200

    Updated reports controller

--- Nemesys/Controllers/ReportsController.cs
+++ Nemesys/Controllers/ReportsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Nemesys.Models;
 using Nemesys.ViewModels;
@@ -11,16 +12,43 @@ namespace Nemesys.Controllers
     public class ReportsController : Controller
     {
         private readonly IReportRepository _reportRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public ReportsController(IReportRepository reportRepository, UserManager<IdentityUser> userManager)
+        {
+            _reportRepository = reportRepository;
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             
-
             var model = new ReportsListViewModel();
             model.Reports = _reportRepository.GetAllReports().OrderByDescending(x => x.Upvotes);
             model.ReportsCount = model.Reports.Count();
-
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var report = _reportRepository.GetReportById(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(report);
+            }
+        }
+
+        [HttpGet]
+        //[Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
     }
 }
---
+++ Nemesys/ViewModels/CreateReportViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemesys.ViewModels
+{
+    public class CreateReportViewModel
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public DateTime DateSpotted { get; set; }
+        public string TypeOfHazard { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+        public int Upvotes { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
