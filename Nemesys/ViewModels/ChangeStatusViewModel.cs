64df09be752b1cc8e10b2aed2bc20c8fbb7b1657
Gayle Cassar <gaylecassar@hotmail.com>
2020-05-25 20:45:38 +0200

    Added change status view

---
+++ Nemesys/ViewModels/ChangeStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nemesys.ViewModels
+{
+    public class ChangeStatusViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Status option is required!")]
+        public int Option { get; set; }
+    }
+}
