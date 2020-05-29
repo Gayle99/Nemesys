c0e45fa8e95d641d921d175007f521eb876f4405
Gayle Cassar <gaylecassar@hotmail.com>
2020-05-29 22:13:22 +0200

    Updated css

--- Nemesys/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Nemesys/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -78,7 +78,7 @@ namespace Nemesys.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Username, Email = Input.Email };
                
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 await _userManager.AddToRoleAsync(user, "Reporter");
